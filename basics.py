64aff938690bfe2cc6f68945472819cdbdd47f7a
unknown <shivanika12@gmail.com>
2019-03-01 23:15:36 +0530

    started learning python

---
+++ basics.py
@@ -0,0 +1,55 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:54:40) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 4+7
+11
+>>> S='HELLO'
+>>> PRINT S
+SyntaxError: invalid syntax
+>>> SyntaxError: invalid syntax
+SyntaxError: invalid syntax
+>>> print s
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(s)?
+>>> s='hello'
+>>> print s
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(s)?
+>>> s=('hello')
+>>> print s
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(s)?
+>>> print (s)
+hello
+>>> s[0]
+'h'
+>>> s[3]
+'l'
+>>> s[-1]
+'o'
+>>> s[::-1]
+'olleh'
+>>> l=[1,2,[3,4,'shivanika']]
+>>> l[2][1]='19'
+>>> l
+[1, 2, [3, '19', 'shivanika']]
+>>> l[1]='sairaj'
+>>> i
+Traceback (most recent call last):
+  File "<pyshell#18>", line 1, in <module>
+    i
+NameError: name 'i' is not defined
+>>> l
+[1, 'sairaj', [3, '19', 'shivanika']]
+>>> [1, 'sairaj', [3, '19', 'shivanika']]
+[1, 'sairaj', [3, '19', 'shivanika']]
+>>> l[1]='shiva'
+>>> l
+[1, 'shiva', [3, '19', 'shivanika']]
+>>> l=[1325t5743231]
+SyntaxError: invalid syntax
+>>> l= [34563]
+>>> sorted l
+SyntaxError: invalid syntax
+>>> sorted (l)
+[34563]
+>>> l.sort()
+>>> l
+[34563]
+>>> 
